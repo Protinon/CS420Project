64a6a9704afe1839644c3dc555301ed911212936
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-10-01 13:56:39 -0400

    Update Printer.java

--- iter1/src/UML/Printer.java
+++ iter1/src/UML/Printer.java
@@ -8,42 +8,37 @@ import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 
-public class Printer implements Printable 
-{
-    final Component comp;
+public class Printer implements Printable {
+	final Component comp;
 
-    public Printer(Component component)
-    {
-        comp = component;
-    }
+	public Printer(Component component) {
+		comp = component;
+	}
 
-    @Override
-    public int print(Graphics g, PageFormat format, int page_index) 
-            throws PrinterException 
-    {
-        if (page_index > 0) 
-        {
-            return Printable.NO_SUCH_PAGE;
-        }
+	@Override
+	public int print(Graphics g, PageFormat format, int page_index) throws PrinterException {
+		if (page_index > 0) {
+			return Printable.NO_SUCH_PAGE;
+		}
 
-        Dimension d = comp.getSize();
-        double compHeight = d.getHeight();
-        double compWidth = d.getWidth();
+		Dimension d = comp.getSize();
+		double compHeight = d.getHeight();
+		double compWidth = d.getWidth();
 
-        double pHeight = format.getImageableHeight();
-        double pWidth = format.getImageableWidth();
+		double pHeight = format.getImageableHeight();
+		double pWidth = format.getImageableWidth();
 
-        double pXStart = format.getImageableX();
-        double pYStart = format.getImageableY();
+		double pXStart = format.getImageableX();
+		double pYStart = format.getImageableY();
 
-        double xRatio = pWidth / compWidth;
-        double yRatio = pHeight / compHeight;
+		double xRatio = pWidth / compWidth;
+		double yRatio = pHeight / compHeight;
 
-        Graphics2D g2 = (Graphics2D) g;
-        g2.translate(pXStart, pYStart);
-        g2.scale(xRatio, yRatio);
-        comp.paint(g2);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.translate(pXStart, pYStart);
+		g2.scale(xRatio, yRatio);
+		comp.paint(g2);
 
-        return Printable.PAGE_EXISTS;
-    }
+		return Printable.PAGE_EXISTS;
+	}
 }
