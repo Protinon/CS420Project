6cfaf4af2dd1038bb021b7d2fb77bc1392242789
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-10-01 14:00:35 -0400

    Update UMLPoint.java

--- iter1/src/UML/UMLPoint.java
+++ iter1/src/UML/UMLPoint.java
@@ -7,25 +7,26 @@ import java.awt.Rectangle;
 
 import javax.swing.JComponent;
 
-public class UMLPoint extends JComponent{
-    
-    public UMLPoint(int x, int y) {
-        super.setBounds(new Rectangle(x, y, 5,5));
-        super.setLocation(x, y);
-       
-        UMLMouseListener listener = new UMLMouseListener();
-        super.addMouseListener(listener);
-        super.addMouseMotionListener(listener);
-    }
+public class UMLPoint extends JComponent {
 
-    @Override
-    protected void paintComponent (Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-   
-        g2.setColor(Color.BLACK);
-        g2.fillOval(0, 0, getWidth(), getHeight());
-        g2.drawOval(0, 0, getWidth(), getHeight());
-        
-    }
+	public UMLPoint(int x, int y) {
+		super.setBounds(new Rectangle(x, y, 5, 5));
+		super.setLocation(x, y);
+
+		UMLMouseListener listener = new UMLMouseListener();
+		super.addMouseListener(listener);
+		super.addMouseMotionListener(listener);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+
+		g2.setColor(Color.BLACK);
+		g2.fillOval(0, 0, getWidth(), getHeight());
+		g2.drawOval(0, 0, getWidth(), getHeight());
+
+	}
 }
+
