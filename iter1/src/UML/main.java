25045f4d16e4948298d53e0917ad9a300c47df44
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-10-01 13:56:03 -0400

    Update main.java

--- iter1/src/UML/main.java
+++ iter1/src/UML/main.java
@@ -1,16 +1,14 @@
 package UML;
 
-import javax.swing.*;
-
 public class main {
 
-public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                UMLController c = new UMLController();
-            }
-        });
-}
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				UMLController c = new UMLController();
+			}
+		});
+	}
 }
