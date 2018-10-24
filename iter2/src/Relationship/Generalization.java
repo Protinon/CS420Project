f0f5a900272926be956ec9fb1e6340847a24074d
Protinon <lukasdeaton@gmail.com>
2018-10-24 19:13:13 -0400

    Added more functionality to relationships, documentation

--- iter2/src/Relationship/Generalization.java
+++ iter2/src/Relationship/Generalization.java
@@ -3,11 +3,13 @@ package Relationship;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 
 import UML.UMLClass;
 
+@SuppressWarnings("serial")
 public class Generalization extends Relationship {
 	
 	public Generalization(UMLClass uClass1, UMLClass uClass2) {
@@ -15,15 +17,9 @@ public class Generalization extends Relationship {
 	}
 
 	@Override
-	public void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		//g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		Rectangle bounds = super.getBounds();
-		System.out.println("Bounds: " + bounds);
-		
+	protected void paintLine(Graphics2D g2, int x1, int y1, int x2, int y2) {
+		g2.setColor(Color.BLUE);
+		g2.drawLine(x1, y1, x2, y2);
 		g2.setColor(Color.BLACK);
-		//g2.drawLine(bounds.x, bounds.y, bounds.width, bounds.height);
-		g2.drawLine(0,  0, bounds.width, bounds.height);
 	}
 }
--- iter2/src/Relationship/Relationship.java
+++ iter2/src/Relationship/Relationship.java
@@ -1,19 +1,21 @@
 package Relationship;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 
 import javax.swing.JComponent;
 
 import UML.UMLClass;
 
+@SuppressWarnings("serial")
 public class Relationship extends JComponent{
 	
-	private final UMLClass uClass1;
-	private final UMLClass uClass2;
-	Point offset1;
-	Point offset2;
-	Point direction;
+	protected final UMLClass uClass1;
+	protected final UMLClass uClass2;
 
 	public Relationship(UMLClass uClass1, UMLClass uClass2) {
 		this.uClass1 = uClass1;
@@ -21,9 +23,38 @@ public class Relationship extends JComponent{
 		uClass1.addComponentListener(new Updater(this));
 		uClass2.addComponentListener(new Updater(this));
 		this.updatePosition(uClass1);
-		setB();
 	}
 	
+	// Paint a line from one corner to the other
+	// based on the component bounds
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		Rectangle bounds = super.getBounds();
+		
+		Point c1 = getCenter(uClass1);
+		Point c2 = getCenter(uClass2);
+		g2.setColor(Color.BLACK);
+
+		if (c1.x > c2.x && c1.y > c2.y || c1.x < c2.x && c1.y < c2.y) {
+			paintLine(g2, 0, 0, bounds.width, bounds.height);
+		}
+		else {
+			paintLine(g2, 0, bounds.height, bounds.width, 0);
+		}
+	}
+	
+	// Subclasses should overload this to paint different types of lines
+	protected void paintLine(Graphics2D g2, int x1, int y1, int x2, int y2) {
+		g2.drawLine(x1, y1, x2, y2);
+	}
+	
+	
+	// Find where uClass1 is relative to uClass2
+	// Get the appropriate edge to put this line on
+	// Call setB() with the found direction
 	public void updatePosition(UMLClass uClass) {
 		UMLClass updatedClass = uClass1;
 		UMLClass staticClass = uClass2;
@@ -32,22 +63,19 @@ public class Relationship extends JComponent{
 			staticClass = uClass1;
 		}
 		
+		// Get two corners to act as vectors extending from the center
 		Point corner1 = staticClass.getLocation();
 		Point corner2 = new Point(corner1.x, corner1.y + staticClass.getHeight());
 		Point center = getCenter(staticClass);
 		
+		// Evaluate the updated class position at both vectors
 		int y = updatedClass.getY();
 		int y1 = evaluate(center, corner1, updatedClass.getLocation());
 		int y2 = evaluate(center, corner2, updatedClass.getLocation());
-		/*System.out.println("Class1 y: " + y);
-		System.out.println("updatedClass: " + updatedClass.getBounds());
-		System.out.println("StaticClass:" + staticClass.getBounds());
-		System.out.println("Corner 1: " + corner1);
-		System.out.println("Corner 2: " + corner2);
-		System.out.println("Center: " + center);
-		System.out.println("Line 1: " + y1);
-		System.out.println("Line 2: " + y2);
-		*/
+
+		// Based on the evaluated y position, 
+		// Figure out which quadrant the moved class is in 
+		Point direction;
 		if (updatedClass.getBounds().intersects(staticClass.getBounds())) {
 			direction = new Point(0, 0);
 		}
@@ -66,28 +94,40 @@ public class Relationship extends JComponent{
 		else {
 			direction = new Point(0, 0);
 		}
-		setB();
+		setB(direction);
 	}
 	
-	public void setB() {
-		Point c1 = getCenter(uClass1);
-		Point p1 = new Point(c1.x + (c1.x - uClass1.getX()) * direction.x, c1.y + (c1.y - uClass1.getY()) * direction.y);
-		Point c2 = getCenter(uClass2);
-		Point p2 = new Point(c2.x + (c2.x - uClass2.getX()) * direction.x * -1, c2.y + (c2.y - uClass2.getY()) * direction.y * -1);
-		Rectangle rect = new Rectangle(p1.x, p1.y, p2.x, p2.y);
-		System.out.println("Direction: " + direction);
-		setBounds(rect);
-		//System.out.println("Bounds: " + rect);
+	// Uses the outer edge of both classes to set the
+	// component position and size
+	private void setB(Point direction) {
+		Point p1 = getEdge(uClass1, direction);
+		Point p2 = getEdge(uClass2, new Point(direction.x * -1, direction.y * -1));
+		int minX = Math.min(p1.x, p2.x);
+		int maxX = Math.max(p1.x, p2.x);
+		int minY = Math.min(p1.y, p2.y);
+		int maxY = Math.max(p1.y, p2.y);
+		Rectangle rect = new Rectangle(minX, minY, maxX - minX, maxY - minY);
 		super.setBounds(rect);
 	}
 	
-	private Point getCenter(UMLClass uClass) {
+	// Given a uClass and direction,
+	// return a point on the outer edge of the class
+	protected Point getEdge(UMLClass uClass, Point direction) {
+		Point center = getCenter(uClass);
+		Point centerOffset = new Point(center.x - uClass.getX(), center.y - uClass.getY());
+		return new Point(center.x + (centerOffset.x * direction.x), center.y + (centerOffset.y * direction.y));
+	}
+	
+	// Get the center point of a uClass
+	protected Point getCenter(UMLClass uClass) {
 		int x = uClass.getX() + uClass.getWidth() / 2;
 		int y = uClass.getY() + uClass.getHeight() / 2;
 		return new Point(x, y);
 	}
 	
-	private int evaluate(Point center, Point corner, Point p) {
+	// Get the slope of a line extending from the center of a class to the corner
+	// Use that to evaluate at point p
+	protected int evaluate(Point center, Point corner, Point p) {
 		float slope = ((float)corner.y - (float)center.y) / ((float)corner.x - (float)center.x);
 		//System.out.println("Slope: " + slope);
 		float diff =  (float)corner.y - ((float)corner.x - (float)p.x) * slope;
