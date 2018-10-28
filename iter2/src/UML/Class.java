36850bbd67a9c6c4f8bf9f9afacc206a00464b97
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-10-27 20:52:44 -0400

    Update Class.java

--- iter2/src/UML/Class.java
+++ iter2/src/UML/Class.java
@@ -12,17 +12,17 @@ import javax.swing.JComponent;
 
 public class Class extends JComponent {
 	private static final long serialVersionUID = -16435718797097829L;
-	
-	int x, y, width = 126, height = 72, boxSize = 24;
-	String name = "",attributes = "", operations = "";
 
-	//set classes x/y coordinate with passed mouse click coordinate
+	int x, y, width = 126, height = 72, boxSize = 24;
+	String name = "Title", attributes = "Attributes", operations = "Operations";
+
+	// set classes x/y coordinate with passed mouse click coordinate
 	public Class(int x1, int y1) {
 		x = x1;
 		y = y1;
 	}
 
-	//check to see if this object contains the passes coordinate
+	// check to see if this object contains the passes coordinate
 	public boolean contains(int x2, int y2) {
 		Rectangle r = new Rectangle(x, y, width, height);
 		if (r.contains(x2, y2)) {
@@ -31,7 +31,7 @@ public class Class extends JComponent {
 		return false;
 	}
 
-	//update this object's location to passed coordinate
+	// update this object's location to passed coordinate
 	public void setLocation(int i, int j) {
 		x = i;
 		y = j;
@@ -39,46 +39,46 @@ public class Class extends JComponent {
 
 	protected void paintClass(Graphics g) {
 		super.paintComponent(g);
-		
-		//draw title box
+
+		// draw title box
 		g.setColor(Color.WHITE);
 		g.fillRect(x, y, width, boxSize);
 		g.setColor(Color.BLACK);
 		g.drawRect(x, y, width, boxSize);
 		g.drawString(name, x + 2, y + 18);
-		
-		//draw attributes box
+
+		// draw attributes box
 		g.setColor(Color.WHITE);
 		g.fillRect(x, y + boxSize, width, height - (boxSize * 2));
 		g.setColor(Color.BLACK);
 		g.drawRect(x, y + boxSize, width, height - (boxSize * 2));
 		g.drawString(attributes, x + 2, y + 42);
 
-		//draw operations box
+		// draw operations box
 		g.setColor(Color.WHITE);
 		g.fillRect(x, y + (boxSize * 2), width, height - (boxSize * 2));
 		g.setColor(Color.BLACK);
 		g.drawRect(x, y + (boxSize * 2), width, height - (boxSize * 2));
 		g.drawString(operations, x + 2, y + 68);
-		
-		//draw box around 3 subboxes ^^
+
+		// draw box around 3 subboxes ^^
 		g.drawRect(x, y, width, height);
 
 	}
-	
-	//update classbox title
+
+	// update classbox title
 	public void setName(String newName) {
 		name = newName;
 		repaint();
 	}
 
-	//update classbox attributes
+	// update classbox attributes
 	public void setAttributes(String newAtts) {
 		attributes = newAtts;
 		repaint();
 	}
 
-	//update classbox operations
+	// update classbox operations
 	public void setOperations(String newOps) {
 		operations = newOps;
 		repaint();
