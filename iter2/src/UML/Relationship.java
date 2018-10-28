1130f51d1be252f9feaf18ad339c9d6de81f06ad
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-10-27 20:08:34 -0400

    Update Relationship.java

--- iter2/src/UML/Relationship.java
+++ iter2/src/UML/Relationship.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 public class Relationship {
 	Class c1, c2;
 	String relationship;
+	int difference;
 
 	public Relationship(String relationship1, Class c0, Class c3) {
 		c1 = c0;
@@ -16,46 +17,51 @@ public class Relationship {
 		// will be coordinates for arrow part of relationship
 		int x0, y0, x1, y1;
 
+		if (relationship != "association") {
+			difference = 20;
+		} else {
+			difference = 0;
+		}
 		if ((c1.x < c2.x)) {
 			if (c1.x + c1.width > c2.x) {
 				if (c1.y > c2.y) {
-					g.drawLine(c1.x + c1.width / 2, c1.y, c2.x + c2.width / 2, c2.y + c2.height + 20);
+					g.drawLine(c1.x + c1.width / 2, c1.y, c2.x + c2.width / 2, c2.y + c2.height + difference);
 					x0 = c2.x + c2.width / 2;
-					y0 = c2.y + c2.height + 20;
+					y0 = c2.y + c2.height + difference;
 					x1 = c2.x + c2.width / 2;
 					y1 = c2.y + c2.height;
 				} else {
-					g.drawLine(c1.x + c1.width / 2, c1.y + c1.height, c2.x + c2.width / 2, c2.y - 20);
+					g.drawLine(c1.x + c1.width / 2, c1.y + c1.height, c2.x + c2.width / 2, c2.y - difference);
 					x0 = c2.x + c2.width / 2;
-					y0 = c2.y - 20;
+					y0 = c2.y - difference;
 					x1 = c2.x + c2.width / 2;
 					y1 = c2.y;
 				}
 			} else {
-				g.drawLine(c1.x + c1.width, c1.y + c1.height / 2, c2.x - 20, c2.y + c2.height / 2);
-				x0 = c2.x - 20;
+				g.drawLine(c1.x + c1.width, c1.y + c1.height / 2, c2.x - difference, c2.y + c2.height / 2);
+				x0 = c2.x - difference;
 				y0 = c2.y + c2.height / 2;
 				x1 = c2.x;
 				y1 = c2.y + c2.height / 2;
 			}
 		} else {
 			if (c2.x + c2.width < c1.x) {
-				g.drawLine(c1.x, c1.y + c1.height / 2, c2.x + c2.width + 20, c2.y + c2.height / 2);
-				x0 = c2.x + c2.width + 20;
+				g.drawLine(c1.x, c1.y + c1.height / 2, c2.x + c2.width + difference, c2.y + c2.height / 2);
+				x0 = c2.x + c2.width + difference;
 				y0 = c2.y + c2.height / 2;
 				x1 = c2.x + c2.width;
 				y1 = c2.y + c2.height / 2;
 			} else {
 				if (c1.y > c2.y) {
-					g.drawLine(c1.x + c1.width / 2, c1.y, c2.x + c2.width / 2, c2.y + c2.height + 20);
+					g.drawLine(c1.x + c1.width / 2, c1.y, c2.x + c2.width / 2, c2.y + c2.height + difference);
 					x0 = c2.x + c2.width / 2;
-					y0 = c2.y + c2.height + 20;
+					y0 = c2.y + c2.height + difference;
 					x1 = c2.x + c2.width / 2;
 					y1 = c2.y + c2.height;
 				} else {
-					g.drawLine(c1.x + c1.width / 2, c1.y + c1.height, c2.x + c2.width / 2, c2.y - 20);
+					g.drawLine(c1.x + c1.width / 2, c1.y + c1.height, c2.x + c2.width / 2, c2.y - difference);
 					x0 = c2.x + c2.width / 2;
-					y0 = c2.y - 20;
+					y0 = c2.y - difference;
 					x1 = c2.x + c2.width / 2;
 					y1 = c2.y;
 				}
