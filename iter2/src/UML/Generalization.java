1e6823cb4300d5808f66f5575fdf4c5073536431
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-11-08 23:50:54 -0500

    Update Generalization.java

--- iter2/src/UML/Generalization.java
+++ iter2/src/UML/Generalization.java
@@ -3,24 +3,39 @@ package UML;
 import java.awt.Graphics;
 
 public class Generalization {
-	int x1, y1, x2, y2, x3, y3;
+	int x1, y1, x2, y2, x3, y3, arrowlength = 16;
 
-	public Generalization(int c1X, int c1Y, int c2X, int c2Y, int x0, int y0, int x4, int y4) {
-		if (((c1X < c2X) && ((c1X + 126) > c2X - 16)) || ((c1X >= c2X) && ((c2X + 126) >= c1X - 16))) {
-			x1 = x0 - 8;
-			x2 = x0 + 8;
+	/**
+	 * Paint method for this class box, overwrites paintComponent.
+	 * 
+	 * @author Bri Long
+	 * @param c1X
+	 * @param c1Y
+	 * @param c2X
+	 * @param c2Y
+	 * @param x0
+	 * @param y0
+	 * @param x4
+	 * @param y4
+	 **/
+	public Generalization(Class c1, Class c2, int x0, int y0, int x4, int y4) {
+		if (((c1.x < c2.x) && ((c1.x + c1.width) > c2.x - arrowlength))
+				|| ((c1.x >= c2.x) && ((c2.x + c1.width) >= c1.x - arrowlength))) {
+			x1 = x0 - arrowlength / 2;
+			x2 = x0 + arrowlength / 2;
 			x3 = x4;
 			y1 = y0;
 			y2 = y0;
 			y3 = y4;
 		}
 
-		if (((c1X >= c2X) && ((c2X + 126) <= c1X - 16)) || ((c1X < c2X) && (c1X + 126 <= c2X - 16))) {
+		if (((c1.x >= c2.x) && ((c2.x + c1.width) <= c1.x - arrowlength))
+				|| ((c1.x < c2.x) && (c1.x + c1.width <= c2.x - arrowlength))) {
 			x1 = x0;
 			x2 = x0;
 			x3 = x4;
-			y1 = y0 + 8;
-			y2 = y0 - 8;
+			y1 = y0 + arrowlength / 2;
+			y2 = y0 - arrowlength / 2;
 			y3 = y4;
 		}
 	}
