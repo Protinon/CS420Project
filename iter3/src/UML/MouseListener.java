4769bfaab93713b45dcfcb7aa1d326b7d3b9a3fc
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-11-11 17:12:24 -0500

    Update MouseListener.java

--- iter3/src/UML/MouseListener.java
+++ iter3/src/UML/MouseListener.java
@@ -6,14 +6,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MouseListener extends MouseAdapter {
-	Class classToRemove;
-	Comment commentToRemove;
 	Controller c;
 	Point p1;
-	boolean aClassIsSelected = false;
-	boolean aCommentIsSelected = false;
-	int classBoxLimit = 20;
-	int commentBoxLimit = 20;
 
 	/**
 	 * Initializes this mouse listener to be associated with a controller.
@@ -38,30 +32,38 @@ public class MouseListener extends MouseAdapter {
 	@Override
 	public void mouseDragged(MouseEvent e) {
 
-		if (aClassIsSelected == true) {
+		if (c.classSelected() == true) {
 			/*
 			 * if user has clicked on a class box, obtain its coordinates, update them from
 			 * the user's mouse's position, and repaint
 			 */
-			if (c.selectedClass.contains(e.getX(), e.getY())) {
+			if (c.getSelectedClass().contains(e.getX(), e.getY())) {
 				int dx = e.getX() - p1.x;
 				int dy = e.getY() - p1.y;
-				if (c.selectedClass.x + dx >= 0 && c.selectedClass.y + dy >= 0
-						&& (c.selectedClass.x + c.selectedClass.width + dx) <= c.getWidth()
-						&& (c.selectedClass.y + c.selectedClass.height + dy) <= c.getHeight()) {
-					for (Class c3 : c.classBoxes) {
-						if (c3 != c.selectedClass) {
-							Rectangle r = new Rectangle(c3.x - 16, c3.y - 16, c3.width + 32, c3.height + 32);
-							if (r.contains(c.selectedClass.x + dx, c.selectedClass.y + dy)
-									|| r.contains(c.selectedClass.x + c.selectedClass.width + dx,
-											c.selectedClass.y + dy)
-									|| r.contains(c.selectedClass.x + c.selectedClass.width + dx,
-											c.selectedClass.y + c.selectedClass.height + dy)
-									|| r.contains(c.selectedClass.x + dx,
-											c.selectedClass.y + c.selectedClass.height + dy)) {
+				if (c.getSelectedClass().getLocation().x + dx >= 0 && c.getSelectedClass().getLocation().y + dy >= 0
+						&& (c.getSelectedClass().getLocation().x + c.getSelectedClass().getWidth() + dx) <= c.getWidth()
+						&& (c.getSelectedClass().getLocation().y + c.getSelectedClass().getHeight() + dy) <= c
+								.getHeight()) {
+					for (Class c3 : c.getClasses()) {
+						if (c3 != c.getSelectedClass()) {
+							Rectangle r = new Rectangle(c3.getLocation().x - 16, c3.getLocation().y - 16,
+									c3.getWidth() + 32, c3.getHeight() + 32);
+							if (r.contains(c.getSelectedClass().getLocation().x + dx,
+									c.getSelectedClass().getLocation().y + dy)
+									|| r.contains(
+											c.getSelectedClass().getLocation().x + c.getSelectedClass().getWidth() + dx,
+											c.getSelectedClass().getLocation().y + dy)
+									|| r.contains(
+											c.getSelectedClass().getLocation().x + c.getSelectedClass().getWidth() + dx,
+											c.getSelectedClass().getLocation().y + c.getSelectedClass().getHeight()
+													+ dy)
+									|| r.contains(c.getSelectedClass().getLocation().x + dx,
+											c.getSelectedClass().getLocation().y + c.getSelectedClass().getHeight()
+													+ dy)) {
 								break;
 							} else {
-								c.selectedClass.setLocation(c.selectedClass.x + dx, c.selectedClass.y + dy);
+								c.getSelectedClass().setLocation(c.getSelectedClass().getLocation().x + dx,
+										c.getSelectedClass().getLocation().y + dy);
 							}
 						}
 					}
@@ -75,14 +77,17 @@ public class MouseListener extends MouseAdapter {
 		 * if user has clicked on a comment box, obtain its coordinates, update them
 		 * from the user's mouse's position, and repaint
 		 */
-		if (aCommentIsSelected == true) {
-			if (c.selectedComment.contains(e.getX(), e.getY())) {
+		if (c.commentSelected() == true) {
+			if (c.getSelectedComment().contains(e.getX(), e.getY())) {
 				int dx = e.getX() - p1.x;
 				int dy = e.getY() - p1.y;
-				if (c.selectedComment.x + dx >= 0 && c.selectedComment.y + dy >= 0
-						&& (c.selectedComment.x + c.selectedComment.width + dx) <= c.getWidth()
-						&& (c.selectedComment.y + c.selectedComment.height + dy) <= c.getHeight()) {
-					c.selectedComment.setLocation(c.selectedComment.x + dx, c.selectedComment.y + dy);
+				if (c.getSelectedComment().getLocation().x + dx >= 0 && c.getSelectedComment().getLocation().y + dy >= 0
+						&& (c.getSelectedComment().getLocation().x + c.getSelectedComment().getWidth() + dx) <= c
+								.getWidth()
+						&& (c.getSelectedComment().getLocation().y + c.getSelectedComment().getHeight() + dy) <= c
+								.getHeight()) {
+					c.getSelectedComment().setLocation(c.getSelectedComment().getLocation().x + dx,
+							c.getSelectedComment().getLocation().y + dy);
 				}
 				p1 = e.getPoint();
 				c.repaint();
@@ -101,166 +106,42 @@ public class MouseListener extends MouseAdapter {
 	 **/
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// store coordinates of user's mouse click
 		p1 = new Point(e.getX(), e.getY());
 
-		if (c.deleteMode == true) {
-			// if user is in delete mode and the mouse clicked inside a class box,
-			// note that we are going to remove that class box
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1.x, p1.y)) {
-					classToRemove = classBox;
-				}
-			}
-
-			// if user is in delete mode and the mouse clicked inside a comment box,
-			// note that we are going to remove that comment box
-			for (Comment commentBox : c.commentBoxes) {
-				if (commentBox.contains(p1.x, p1.y)) {
-					commentToRemove = commentBox;
-				}
-			}
-
-			// remove classes and comments that we have clicked on while in delete mode
-			c.classBoxes.remove(classToRemove);
-			c.commentBoxes.remove(commentToRemove);
-			c.repaint();
+		if (c.deleteMode() == true) {
+			c.deleteObject(p1);
 		}
 
-		if (c.classMode == true) {
-			// if user is in class mode and there are less than 20 class boxes on
-			// the screen, then create a new class box at the user's mouse's coordinates
-			if (c.classBoxes.size() < classBoxLimit) {
-				c.classBoxes.add(new Class(p1.x, p1.y));
-				c.repaint();
-			}
+		if (c.classMode() == true) {
+			c.addClass(p1);
 		}
 
-		if (c.commentMode == true) {
-			// if user is in comment mode and there are less than 20 comment boxes on
-			// the screen, then create a new comment box at the user's mouse's coordinates
-			if (c.commentBoxes.size() < commentBoxLimit) {
-				c.commentBoxes.add(new Comment(p1.x, p1.y));
-				c.repaint();
-			}
+		if (c.commentMode() == true) {
+			c.addComment(p1);
 		}
 
-		if (c.associationMode == true) {
-			// if user is in association mode, check if there is a class box
-			// at the coordinate of the mouse click and store that box to have an
-			// association drawn(association will be drawn between first and second
-			// class boxes clicked (if an odd number of boxes clicked, the last box will
-			// not have a relationship until an even number completes it)
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1.x, p1.y)) {
-					c.associatedClasses.add(classBox);
-					c.repaint();
-				}
-			}
+		if (c.associationMode() == true) {
+			c.addAssociation(p1);
 		}
 
-		// if user is in generalization mode, check if there is a class box
-		// at the coordinate of the mouse click and store that box to have a
-		// generalization drawn(generalization will be drawn between first and second
-		// class boxes clicked (if an odd number of boxes clicked, the last box will
-		// not have a relationship until an even number completes it)
-		if (c.generalizationMode == true) {
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1.x, p1.y)) {
-					c.generalizedClasses.add(classBox);
-					c.repaint();
-				}
-			}
+		if (c.generalizationMode() == true) {
+			c.addGeneralization(p1);
 		}
 
-		// if user is in dependency mode, check if there is a class box
-		// at the coordinate of the mouse click and store that box to have a
-		// dependency drawn(dependency will be drawn between first and second
-		// class boxes clicked (if an odd number of boxes clicked, the last box will
-		// not have a relationship until an even number completes it)
-		if (c.dependencyMode == true) {
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1.x, p1.y)) {
-					c.dependedClasses.add(classBox);
-					c.repaint();
-				}
-			}
-
+		if (c.dependencyMode() == true) {
+			c.addDependency(p1);
 		}
 
-		// if user is in aggregation mode, check if there is a class box
-		// at the coordinate of the mouse click and store that box to have an
-		// aggregation drawn(aggregation will be drawn between first and second
-		// class boxes clicked (if an odd number of boxes clicked, the last box will
-		// not have a relationship until an even number completes it)
-		if (c.aggregationMode == true) {
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1.x, p1.y)) {
-					c.aggregatedClasses.add(classBox);
-					c.repaint();
-				}
-			}
+		if (c.aggregationMode() == true) {
+			c.addAggregation(p1);
 		}
 
-		// if user is in composition mode, check if there is a class box
-		// at the coordinate of the mouse click and store that box to have a
-		// composition drawn(composition will be drawn between first and second
-		// class boxes clicked (if an odd number of boxes clicked, the last box will
-		// not have a relationship until an even number completes it)
-		if (c.compositionMode == true) {
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1.x, p1.y)) {
-					c.compositedClasses.add(classBox);
-					c.repaint();
-				}
-			}
+		if (c.compositionMode() == true) {
+			c.addComposition(p1);
 		}
 
-		if (c.selectMode == true) {
-			// if user is in select mode, and the mouse click's coordinates
-			// are contained within a class box, that box will be moveable by dragging
-			for (Class classBox : c.classBoxes) {
-				if (classBox.contains(p1)) {
-					// set the "inspector-like" functions to visible
-					c.selectedClass = classBox;
-					aClassIsSelected = true;
-					c.v.okayButton.setVisible(true);
-					c.v.titleLabel.setVisible(true);
-					c.v.title.setText(c.selectedClass.name);
-					c.v.title.setVisible(true);
-					c.v.attsLabel.setVisible(true);
-					c.v.atts.setText(c.selectedClass.attributes);
-					c.v.atts.setVisible(true);
-					c.v.opsLabel.setVisible(true);
-					c.v.ops.setText(c.selectedClass.operations);
-					c.v.ops.setVisible(true);
-					break;
-				} else {
-					// a class is not selected so make "inspector-like" function invisible
-					c.selectedClass = null;
-					aClassIsSelected = false;
-					c.v.okayButton.setVisible(false);
-					c.v.titleLabel.setVisible(false);
-					c.v.title.setVisible(false);
-					c.v.attsLabel.setVisible(false);
-					c.v.atts.setVisible(false);
-					c.v.opsLabel.setVisible(false);
-					c.v.ops.setVisible(false);
-				}
-			}
-
-			// if user is in select mode, and the mouse click's coordinates
-			// are contained within a comment box, that box will be moveable by dragging
-			for (Comment commentBox : c.commentBoxes) {
-				if (commentBox.contains(p1.x, p1.y)) {
-					c.selectedComment = commentBox;
-					aCommentIsSelected = true;
-					break;
-				} else {
-					aCommentIsSelected = false;
-				}
-			}
+		if (c.selectMode() == true) {
+			c.selectObject(p1);
 		}
 	}
 }
-
