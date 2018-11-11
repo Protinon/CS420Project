8e4b63bd839a92435f0f57d4913f2c19b8a5b6f4
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-11-11 17:08:16 -0500

    Update Dependency.java

--- iter3/src/UML/Dependency.java
+++ iter3/src/UML/Dependency.java
@@ -4,30 +4,13 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Dependency {
-	int x1, y1, x2, y2, x3, y3, arrowlength = 16;
-	Class c1, c2;
-	double slope;
+	private int x1, y1, x2, y2;
 
-	/**
-	 * Paint method for this class box, overwrites paintComponent.
-	 * 
-	 * @author Bri Long
-	 * @param c1X
-	 * @param c1Y
-	 * @param c2X
-	 * @param c2Y
-	 * @param x0
-	 * @param y0
-	 * @param x4
-	 * @param y4
-	 **/
 	public Dependency(Class c1, Class c2, int x0, int y0, int x4, int y4) {
 		x1 = x0;
 		y1 = y0;
 		x2 = x4;
 		y2 = y4;
-		x3 = x0;
-		y3 = y0;
 	}
 
 	public void paintDependency(Graphics g) {
@@ -54,5 +37,4 @@ public class Dependency {
 			}
 		}
 	}
-
 }
