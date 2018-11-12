bb85d97ab54badcac0818946f8d1b1e6713b9489
bmlong1 <41707083+bmlong1@users.noreply.github.com>
2018-11-12 11:15:46 -0500

    Add files via upload

--- iter3/Action/AddCompositionAction.java
+++ iter3/Action/AddCompositionAction.java
@@ -3,21 +3,22 @@ package UML;
 import java.awt.Point;
 import java.util.ArrayList;
 
-public class AddGeneralizationAction implements Action {
+public class AddCompositionAction implements Action {
 	private Point p;
 	private ArrayList<Class> associations;
 	private Class c;
-	public AddGeneralizationAction(Class c, Point p, ArrayList<Class> associations) {
+
+	public AddCompositionAction(Class c, Point p, ArrayList<Class> associations) {
 		this.associations = associations;
 		this.p = p;
 		this.c = c;
-		
+
 	}
-	
+
 	public void doAction() {
 		associations.add(c);
 	}
-	
+
 	public void undoAction() {
 		associations.remove(c);
 	}
